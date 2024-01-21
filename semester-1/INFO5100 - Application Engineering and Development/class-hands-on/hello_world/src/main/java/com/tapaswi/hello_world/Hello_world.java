b02fd2e8a379d16f40f01721e0bf9f903d6025c9
Tapaswi <satyapanthi.t@northeastern.edu>
2024-01-21 13:10:32 -0500

    initial commit

---
+++ semester-1/INFO5100 - Application Engineering and Development/class-hands-on/hello_world/src/main/java/com/tapaswi/hello_world/Hello_world.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tapaswi.hello_world;
+
+/**
+ *
+ * @author tapas
+ */
+public class Hello_world {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
